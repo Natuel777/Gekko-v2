2a03785e7d05f82e80eac806dc51c3af3282fa81
iara <iaramarielmartinez@hotmail.com>
2026-09-20 19:58:42 -0300

    Update ScreenReafirm.cs

--- Assets/Scripts/Canvas/Menu/Pause/ScreenReafirm.cs
+++ Assets/Scripts/Canvas/Menu/Pause/ScreenReafirm.cs
@@ -5,9 +5,10 @@ public class ScreenReafirm : Screens
 {
     public void BTN_Menu()
     {
-        AudioManager.instance.Play(SoundNames.UiButton);
+        if(AudioManager.instance) AudioManager.instance.Play(SoundNames.UiButton);
         if(GameManager.Instance)
         GameManager.Instance.IsPause = false;
         SceneManager.LoadScene(ScenesDictionary.SceneName[ScenesNames.Menu]);
+        if (AudioManager.instance) AudioManager.instance.ResetAudio();
     }
 }
